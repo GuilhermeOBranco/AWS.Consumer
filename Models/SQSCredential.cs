12bc268ef69a8df43cf6a4b4095ac776cbd94628
Guilherme <guilhermebranco800@gmail.com>
2022-06-28 22:30:14 -0300

    .

--- Models/SQSCredential.cs
+++ Models/SQSCredential.cs
@@ -2,6 +2,10 @@ namespace AWS.Consumer.Models
 {
     public class SQSCredential
     {
+        /// <summary>
+        /// Agra ta assim
+        /// </summary>
+        /// <value></value>
         public string AccessKey { get; set; }
         public string SecretAccessKey { get; set; }
         public string SQSURL { get; set; }
